b3716ad51beefc122321aad2989d810d3d1caa79
Trần Nguyên Khang <khangtran722001@gmail.com>
2023-06-06 20:48:20 +0700

    Khang tạo project, template và db

---
+++ TiKayStore/TiKayStore/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TiKayStore.MvcApplication" Language="C#" %>
---
+++ TiKayStore/TiKayStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TiKayStore.Startup))]
+namespace TiKayStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
